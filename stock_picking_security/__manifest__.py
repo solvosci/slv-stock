ce24545827fbce10f40358b42165c6cd06d42267
ChristianSantamaria <christiancameselle@hotmail.com>
2021-02-11 10:01:50 +0100

    [ADD] stock_picking_security

---
+++ stock_picking_security/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Stock Picking Security",
+    "summary": """
+        Add a group with reduced permissions for partners in Stock.
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "stock",
+    "website": "https://github.com/solvosci/slv-stock",
+    "depends": [
+        "stock"
+    ],
+    "data": [
+        "security/stock_security.xml"
+    ],
+    'installable': True,
+}
