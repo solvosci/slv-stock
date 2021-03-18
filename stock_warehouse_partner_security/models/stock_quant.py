f81c2c4984034d573b1cc4fb7b8bc122ba1a6e7b
David Alonso // Solvos <david.alonso@solvos.es>
2021-03-18 17:25:39 +0100

    [FIX] stock_warehouse_partner_security: administrators see only their own location in My Inventory Report

--- stock_warehouse_partner_security/models/stock_quant.py
+++ stock_warehouse_partner_security/models/stock_quant.py
@@ -13,6 +13,20 @@ class StockQuant(models.Model):
     )
     
     @api.model
+    def _get_custom_locations(self):
+        my_location_ids = []
+        if self.env.user.property_stock_supplier:
+            my_location_ids.append(
+                self.env.user.property_stock_supplier.id
+            )
+        if self.env.user.parent_id and \
+            self.env.user.parent_id.property_stock_supplier:
+            my_location_ids.append(
+                self.env.user.parent_id.property_stock_supplier.id
+            )            
+        return my_location_ids
+
+    @api.model
     def create(self, vals):
         """
         Creation record rules are bypassed by default, and we need disabling 
@@ -23,17 +37,7 @@ class StockQuant(models.Model):
             and self.user_has_groups(
                 'stock_warehouse_partner_security.group_stock_picking_partner'
             ) and not self.user_has_groups('stock.group_stock_manager'):
-            location_allowed = (
-                self.env.user.property_stock_supplier and
-                self.env.user.property_stock_supplier.id == \
-                    vals['location_id']
-            ) or (
-                self.env.user.parent_id and
-                self.env.user.parent_id.property_stock_supplier and
-                self.env.user.parent_id.property_stock_supplier.id == \
-                    vals['location_id']
-            )
-            if not location_allowed:
+            if not vals['location_id'] in self._get_custom_locations():
                 raise ValidationError(
                     _("Location not allowed: %s") %
                         self.env["stock.location"].browse(
@@ -50,3 +54,17 @@ class StockQuant(models.Model):
                 'stock_warehouse_partner_security.group_stock_picking_partner'
             )
         )
+
+    @api.model
+    def _get_quants_action(self, domain=None, extend=False):
+        if self.env.context.get("show_my_inventory", False):
+            my_location_ids = self._get_custom_locations()
+            # Prevent accessing to every location when no locations were 
+            # defined for this user
+            if len(my_location_ids) == 0:
+                my_location_ids.append(0)
+
+            my_domain = [("location_id", "in", my_location_ids)]
+            domain = (domain and domain + my_domain) or my_domain
+
+        return super()._get_quants_action(domain=domain, extend=extend)
