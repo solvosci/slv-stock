683a8ef25dc4d6e3fd29b074e3d3234600f1432f
David Alonso // Solvos <david.alonso@solvos.es>
2021-10-07 10:49:32 +0200

    [ADD] stock_move_action_done_custdate

---
+++ stock_move_action_done_custdate/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Stock Move Custom Date when done",
+    "summary": """
+        Enables passing a custom date for a stock move when move is marked
+        as done
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "stock",
+    "website": "https://github.com/solvosci/slv-stock",
+    "depends": ["stock"],
+    "data": [],
+    'installable': True,
+}
