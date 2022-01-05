96b69325b214a9a69cb1b0ba4daa2fb5b0ab874e
David Alonso // Solvos <david.alonso@solvos.es>
2022-01-05 11:10:42 +0100

    [ADD] stock_picking_report_deliv_vat

---
+++ stock_picking_report_deliv_vat/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Stock Picking Delivery Document - Partner/Customer VAT",
+    "summary": """
+        Adds customer/vendor VAT to picking delivery report
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Warehouse",
+    "website": "https://github.com/solvosci/slv-stock",
+    "depends": ["stock"],
+    "data": ["views/report_delivery_document.xml"],
+    'installable': True,
+}
