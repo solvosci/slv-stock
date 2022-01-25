0f2667998687e6aa38f719849ea63dd1f3ba0203
ChristianSantamaria <christiancameselle@hotmail.com>
2022-01-25 08:57:12 +0100

    [IMP] stock_picking_mgmt_weight: some improvements
    - Add the license plate towing in the tree view of the scales
    - Add shipping resource in the tree view of purchase order lines
    - Add an optional hide in the duplicate fields of received, invoiced and classified
    - Add the scale ticket number and the vehicle license plate in the "See classification" wizard
    - Add the license plate towing to the ticket report if it has been filled out

--- stock_picking_mgmt_weight/__manifest__.py
+++ stock_picking_mgmt_weight/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.1.2.0",
+    "version": "13.0.1.3.1",
     "category": "stock",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": [
