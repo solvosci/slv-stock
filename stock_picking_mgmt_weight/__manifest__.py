3cebac84e987ddc4ada83eaf2b333b2dd7abb375
David Alonso // Solvos <david.alonso@solvos.es>
2022-02-07 08:53:04 +0100

    [FIX] stock_picking_mgmt_weight: disable expected_qty calculation

--- stock_picking_mgmt_weight/__manifest__.py
+++ stock_picking_mgmt_weight/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.1.4.1",
+    "version": "13.0.1.4.2",
     "category": "stock",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": [
