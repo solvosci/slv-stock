550c178c77aaf5a668a0e9a972c2fa612706ac2d
David Alonso // Solvos <david.alonso@solvos.es>
2022-02-10 11:34:16 +0100

    [IMP] stock_picking_mgmt_weight: sale lines menu - new pending column and sum aggregates
    * Added new computed pending quantity field, which is shown in this menu.
    * Simplified some quantity column titles.
    * Added aggregate sum values for quantity columns.

--- stock_picking_mgmt_weight/models/sale_order_line.py
+++ stock_picking_mgmt_weight/models/sale_order_line.py
@@ -7,5 +7,14 @@ from odoo import fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
+    pending_qty = fields.Float(
+        compute="_compute_pending_qty",
+        digits="Product Unit of Measure",
+    )
+
     supply_condition_id = fields.Many2one(comodel_name="supply.condition")
     vehicle_type_id = fields.Many2one(comodel_name="vehicle.type")
+
+    def _compute_pending_qty(self):
+        for line in self:
+            line.pending_qty = line.product_uom_qty - line.qty_delivered
