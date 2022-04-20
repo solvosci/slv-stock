9c1b281fde8df8bc1da892e4917e8deee0ca92de
ChristianSantamaria <christiancameselle@hotmail.com>
2022-04-20 17:18:50 +0200

    [IMP] stock_picking_mgmt_weight: pending_qty store in sale order line

--- stock_picking_mgmt_weight/__manifest__.py
+++ stock_picking_mgmt_weight/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.1.7.0",
+    "version": "13.0.1.8.0",
     "category": "stock",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": [
