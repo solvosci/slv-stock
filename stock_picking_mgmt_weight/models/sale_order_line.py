37b660b29c044b14fd896ca04d0bb1cc5b0b9630
ChristianSantamaria <christiancameselle@hotmail.com>
2022-04-20 17:18:50 +0200

    [IMP] stock_picking_mgmt_weight: pending_qty store in sale order line

--- stock_picking_mgmt_weight/models/sale_order_line.py
+++ stock_picking_mgmt_weight/models/sale_order_line.py
@@ -1,7 +1,7 @@
 # © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class SaleOrderLine(models.Model):
@@ -10,11 +10,13 @@ class SaleOrderLine(models.Model):
     pending_qty = fields.Float(
         compute="_compute_pending_qty",
         digits="Product Unit of Measure",
+        store=True
     )
 
     supply_condition_id = fields.Many2one(comodel_name="supply.condition")
     vehicle_type_id = fields.Many2one(comodel_name="vehicle.type")
 
+    @api.depends('product_uom_qty', 'qty_delivered')
     def _compute_pending_qty(self):
         for line in self:
             line.pending_qty = line.product_uom_qty - line.qty_delivered
