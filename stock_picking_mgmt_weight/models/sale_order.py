840ad5f0d1b29accb9d281f296490d59fabbca8f
David Alonso // Solvos <david.alonso@solvos.es>
2022-05-18 18:14:05 +0200

    [IMP] stock_picking_mgmt_weight: sale order pending quantities cancellation

--- stock_picking_mgmt_weight/models/sale_order.py
+++ stock_picking_mgmt_weight/models/sale_order.py
@@ -7,7 +7,43 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    has_pending_qty = fields.Boolean(
+        compute="_compute_has_pending_qty",
+        help="Instrumental field indicating if there's any pending quantity"
+        " for this order",
+    )
     shipping_resource_id = fields.Many2one(
         comodel_name="shipping.resource",
         default=lambda self: self.env["shipping.resource"].get_default(),
     )
+
+    def _compute_has_pending_qty(self):
+        for record in self:
+            record.has_pending_qty = any([
+                line.has_pending_qty for line in record.order_line
+            ])
+
+    def action_cancel_pending(self):
+        """
+        Finishes selected orders, cancelling pending quantities.
+        Pending pickings are cancelled as result of this process, not vice versa
+        """
+        # An error is not raised due to batch action with several orders launch
+        # In that case, if orders with no pending quantities are selected, this
+        #  process is simply ignored
+        if not self.has_pending_qty:
+            return
+
+        pending_lines = self.order_line.filtered(lambda x: x.has_pending_qty)
+        for line in pending_lines:
+            # TODO better get it from to-cancel pickings?
+            line.qty_cancelled += line.pending_qty
+
+        cancel_pickings = pending_lines.mapped(
+            "move_ids.picking_id"
+        ).filtered(lambda x: x.state not in ["done", "cancel"])
+        cancel_pickings.action_cancel()
+
+    def action_cancel_pending_multi(self):
+        for record in self.browse(self.env.context["active_ids"]):
+            record.action_cancel_pending()
--- stock_picking_mgmt_weight/models/sale_order_line.py
+++ stock_picking_mgmt_weight/models/sale_order_line.py
@@ -2,21 +2,46 @@
 # License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
 
 from odoo import fields, models, api
+from odoo.tools.float_utils import float_compare
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
+    qty_cancelled = fields.Float(
+        digits="Product Unit of Measure",
+        string="Cancelled Quantity",
+        readonly=True,
+        help="Quantities affected by 'Cancel Pending' process",
+    )
     pending_qty = fields.Float(
         compute="_compute_pending_qty",
+        string="Pending Quantity",
         digits="Product Unit of Measure",
-        store=True
+        store=True,
+    )
+    has_pending_qty = fields.Boolean(
+        compute="_compute_pending_qty",
+        store=True,
+        readonly=True,
+        help="Technical field for sale order has pending quantity computation",
     )
 
     supply_condition_id = fields.Many2one(comodel_name="supply.condition")
     vehicle_type_id = fields.Many2one(comodel_name="vehicle.type")
 
-    @api.depends('product_uom_qty', 'qty_delivered')
+    @api.depends("product_uom_qty", "qty_delivered", "qty_cancelled")
     def _compute_pending_qty(self):
         for line in self:
-            line.pending_qty = line.product_uom_qty - line.qty_delivered
+            line.pending_qty = (
+                line.product_uom_qty - line.qty_delivered - line.qty_cancelled
+            )
+            # We only take in account positive pending quantities
+            #  (it's possible to be negative)
+            line.has_pending_qty = (
+                float_compare(
+                    line.pending_qty,
+                    0,
+                    precision_rounding=line.product_uom.rounding or 0.001
+                ) == 1
+            )
