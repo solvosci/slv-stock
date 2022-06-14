b10a4eeafa4fd2ce2f006c175fe4c4b467941d6e
David Alonso // Solvos <david.alonso@solvos.es>
2022-06-14 17:33:14 +0200

    [IMP] stock_picking_mgmt_weight: added extended invoice status to RFQ tree

--- stock_picking_mgmt_weight/__manifest__.py
+++ stock_picking_mgmt_weight/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.1.12.0",
+    "version": "13.0.1.13.0",
     "category": "stock",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": [
