bb137c1f7ab91692c4cb0eb25bf130b19affa5a4
David Alonso // Solvos <david.alonso@solvos.es>
2022-07-15 13:36:17 +0200

    [FIX] stock_picking_mgmt_weight: prevent classification validation concurrency

--- stock_picking_mgmt_weight/__manifest__.py
+++ stock_picking_mgmt_weight/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.1.14.0",
+    "version": "13.0.1.14.1",
     "category": "stock",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": [
