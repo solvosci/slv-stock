9c90bf502aa6992c48ec0ce8d7179dd75cfc8740
David Alonso // Solvos <david.alonso@solvos.es>
2022-12-26 20:11:51 +0100

    [UPD] stock_picking_mgmt_weight: switch scale protocol to continuous -S- mode

--- stock_picking_mgmt_weight/controllers/scale.py
+++ stock_picking_mgmt_weight/controllers/scale.py
@@ -14,11 +14,31 @@ class ScaleController(http.Controller):
     )
     def read(self):
         # TODO test it in a multi-company environment and user
+        # scale_id = request.env['res.users'].search([
+        #     ('id', '=', request.session.uid)
+        # ]).company_id.picking_operations_scale_id
+
+        # return scale_id and scale_id.sudo().get_weight() or {
+        #     "value": "----",
+        #     "err": _("Undefined scale for current company")
+        # }
         scale_id = request.env['res.users'].search([
             ('id', '=', request.session.uid)
-        ]).company_id.picking_operations_scale_id
-
-        return scale_id and scale_id.sudo().get_weight() or {
-            "value": "----",
-            "err": _("Undefined scale for current company")
-        }
+        ]).company_id.sudo().picking_operations_scale_id
+        if not scale_id:
+            return {
+                "value": "----",
+                "err": _("Undefined scale for current company"),
+            }
+        ret = request.env["scale.scale"]._run_scheduler_once(scale_id)
+        if ret.get("last_weight_error", False):
+            return {
+                "value": "----",
+                "err": ret["last_weight_error"],
+            }
+        else:
+            return {
+                "value": ret["last_weight"],
+                "ok": ret["last_weight_ok"],
+                "err": False,
+            }
