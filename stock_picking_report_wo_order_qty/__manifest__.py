b9158661006c84964bb86919ab7ff20d9ca96ebd
LuPinheiro <gerallupinheiro@gmail.com>
2023-01-09 11:18:54 +0100

    [ADD] stock_picking_report_wo_order_qty

---
+++ stock_picking_report_wo_order_qty/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Stock Picking Report Without Order Quantity",
+    "summary": """
+        Hide delivery slip note report quantity field
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Stock",
+    "website": "https://github.com/solvosci/slv-stock",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "report/report_deliveryslip.xml",
+    ],
+    'installable': True,
+}
