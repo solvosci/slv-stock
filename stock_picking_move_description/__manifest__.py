98db1ad4305df536b1d391dd665dddbeda3d4a16
LuPinheiro <gerallupinheiro@gmail.com>
2023-02-01 13:11:47 +0100

    [IMP] stock_picking_move_description: added description to move kanban view

--- stock_picking_move_description/__manifest__.py
+++ stock_picking_move_description/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "15.0.1.0.1",
+    "version": "15.0.1.1.0",
     "category": "Inventory/Inventory",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": ["stock"],
--- stock_picking_move_description/readme/CONTRIBUTORS.rst
+++ stock_picking_move_description/readme/CONTRIBUTORS.rst
@@ -1,1 +1,2 @@
 * David Alonso <david.alonso@solvos.es>
+* Lucia Pinheiro <lucia.pinheiro@solvos.es>
