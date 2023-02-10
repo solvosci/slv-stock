cb94a5891ca48acf061ad1a8652b7e898ad99565
David Alonso // Solvos <david.alonso@solvos.es>
2023-02-10 18:49:55 +0100

    [IMP] stock_valuation: several visual improvements
    In this commit these new features were added:
    * Product template form view, average prices by warehouse are shown if is single-variant.
    * Product average price tree view, stock quantity is shown and new zero stock filters are added.
      Also, new colors depending on stock quantity are used.
    * Product average price form view, stock quantity is shown.
    * Stock quant view, unit measure for quant is improved, and price is shown.
    * Product average price at date, filtered by default for non-zero quant records.

--- stock_valuation/models/stock_quant.py
+++ stock_valuation/models/stock_quant.py
@@ -1,11 +1,17 @@
 # © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
 
-from odoo import models
+from odoo import fields, models
 
 
 class StockQuant(models.Model):
     _inherit = "stock.quant"
+
+    average_price = fields.Monetary(
+        string="Price",
+        compute="_compute_value",
+        groups="stock.group_stock_manager",
+    )
 
     def _compute_value(self):
         """
@@ -31,11 +37,11 @@ class StockQuant(models.Model):
             # TODO check speed process (warehouse detection is poor implemented)
             for quant in average_quants:
                 warehouse = quant.location_id.get_warehouse()
-                quant.value = (
-                    quant.quantity *
-                    (
-                        dict_ap.get(quant.product_id.id, False)
-                        and dict_ap[quant.product_id.id].get(warehouse.id, 0.0)
-                        or 0.0
-                    )
+                quant.average_price = (
+                    dict_ap.get(quant.product_id.id, False)
+                    and dict_ap[quant.product_id.id].get(warehouse.id, 0.0)
+                    or 0.0
                 )
+                quant.value = quant.quantity * quant.average_price
+        for other_quant in (self - average_quants):
+            other_quant.average_price = other_quant.product_id.standard_price
