d5acf5dec84567d4b1cee2af74261517ce913772
David Alonso // Solvos <david.alonso@solvos.es>
2023-03-02 10:00:29 +0100

    [FIX] stock_valuation: purchase order line price change to zero coverage
    Before this fix, when a POL price was changed to 0.0, price recalculation was
    skipped. Since 0.0 is a price that should be allowed, this fix includes it.

--- stock_valuation/__manifest__.py
+++ stock_valuation/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.3.2.1",
+    "version": "13.0.3.2.2",
     "category": "stock",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": [
