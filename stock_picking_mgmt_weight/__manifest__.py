de6306534568db4bc65c81832e55672f3717c1d5
David Alonso // Solvos <david.alonso@solvos.es>
2023-06-14 16:51:20 +0200

    [FIX] stock_picking_mgmt_weight: purchase order line first invoice date - based on accounting date
    Before this fix, for this calculation bill date was used, but accounting date
    is more accurate.

--- stock_picking_mgmt_weight/__manifest__.py
+++ stock_picking_mgmt_weight/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.1.21.0",
+    "version": "13.0.1.21.1",
     "category": "stock",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": [
