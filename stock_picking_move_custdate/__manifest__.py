97c9a3ad2a7ef815704ca1f3b3ac720a6b5ff742
David Alonso // Solvos <david.alonso@solvos.es>
2023-09-09 11:58:45 +0200

    [ADD] stock_picking_move_custdate
    Passes picking scheduled date to stock moves when are done.
    
    This is is applied for both move and move lines, and for picking done date.

---
+++ stock_picking_move_custdate/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Stock Move Custom Date when done - link with pickings",
+    "summary": """
+        Passes picking scheduled date to stock moves when are done
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "website": "https://github.com/solvosci/slv-stock",
+    "depends": ["stock_move_action_done_custdate"],
+    'installable': True,
+}
