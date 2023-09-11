211e43f06415c2a90d6cd1b9ea8950919dd2028e
Christian Santamaria <christian.santamaria@solvos.es>
2023-09-11 13:06:52 +0200

    [IMP] fcd: storable fao subzone name

--- fcd/models/fcd_fao_subzone.py
+++ fcd/models/fcd_fao_subzone.py
@@ -1,14 +1,14 @@
 # © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class FaoSubZone(models.Model):
     _name = 'fcd.fao.subzone'
     _description = 'fcd.fao.subzone'
 
-    name = fields.Char(compute='_compute_name')
+    name = fields.Char(compute='_compute_name', store=True)
     subzone_code = fields.Char()
     division_name = fields.Char()
     subzone = fields.Char()
@@ -18,6 +18,7 @@ class FaoSubZone(models.Model):
     zone_code = fields.Char()
     zone_name = fields.Char()
 
+    @api.depends('zone_code', 'zone_name', 'subzone', 'division', 'division_name')
     def _compute_name(self):
         for record in self:
             if record.division:
