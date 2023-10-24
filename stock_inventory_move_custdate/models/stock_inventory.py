d10a4c011dc5f74d662704e45ae2983427004d73
David Alonso // Solvos <david.alonso@solvos.es>
2023-10-24 17:16:10 +0200

    [IMP] stock_inventory_move_custdate: quantity at date support
    With this improvement, quantity at date is shown and difference
    could be calculated based on it

---
+++ stock_inventory_move_custdate/models/stock_inventory.py
@@ -0,0 +1,24 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models
+
+
+class StockInventory(models.Model):
+    _inherit = "stock.inventory"
+
+    def action_state_to_in_progress(self):
+        super().action_state_to_in_progress()
+        self.stock_quant_ids.update({"current_inventory_id": self.id})
+
+    def action_state_to_done(self):
+        super().action_state_to_done()
+        self.stock_quant_ids.update({"current_inventory_id": False})
+
+    def action_state_to_draft(self):
+        self.stock_quant_ids.update({"current_inventory_id": False})
+        super().action_state_to_draft()
+
+    def action_view_inventory_adjustment(self):
+        result = super().action_view_inventory_adjustment()
+        result["context"]["hide_quantity_at_date"] = False
+        return result
