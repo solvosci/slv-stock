c0bebd3f04bc98806518f27ecbcde386d71ce8fb
Christian Santamaria <christian.santamaria@solvos.es>
2023-12-22 12:32:17 +0100

    [IMP] stock_barcodes_gs1_secondary_unit: change onchage method

--- stock_barcodes_gs1_secondary_unit/__manifest__.py
+++ stock_barcodes_gs1_secondary_unit/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "category": "Stock",
     "website": "",
     "depends": [
