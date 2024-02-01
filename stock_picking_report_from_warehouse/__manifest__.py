2d0328a7a468fab6c4d8ad4978a9fd9327a1c901
Christian Santamaria <christian.santamaria@solvos.es>
2024-02-01 15:40:16 +0100

    [ADD] stock_picking_report_from_warehouse

---
+++ stock_picking_report_from_warehouse/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Stock Picking Report From Warehouse",
+    "summary": """
+        Adds from warehouse address in delivery report
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Inventory",
+    "website": "https://github.com/solvosci/slv-stock",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "report/stock_picking_template.xml",
+    ],
+    "installable": True,
+}
