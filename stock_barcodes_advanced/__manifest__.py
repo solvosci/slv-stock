3a968e424c3ffc6df42b5169c89ad732147a35c5
Christian Santamaria <christian.santamaria@solvos.es>
2024-02-14 13:00:40 +0100

    [IMP] stock_barcodes_advanced: some improvements
    - Adds qty demand and done in kanban top
    - Adds new name_get with "LOT (QTY unit)"

--- stock_barcodes_advanced/__manifest__.py
+++ stock_barcodes_advanced/__manifest__.py
@@ -7,10 +7,12 @@
             - Refresh fields after scan
             - Update quantity pending after scan
             - Change barcode button on stock.picking to only confirmed ones
+            - Adds qty demand and done in kanban top
+            - Adds new name_get with "LOT (QTY unit)"
     """,
     "author": "Solvos",
     "license": "AGPL-3",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "category": "Stock",
     "website": "",
     "depends": [
@@ -18,6 +20,7 @@
     ],
     "data": [
         "views/stock_picking_views.xml",
+        "wizard/stock_barcodes_read_picking_views.xml",
     ],
     "installable": True,
 }
--- stock_barcodes_advanced/wizard/stock_barcodes_read_picking.py
+++ stock_barcodes_advanced/wizard/stock_barcodes_read_picking.py
@@ -1,7 +1,7 @@
 # © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License AGPL-3.0 (http://www.gnu.org/licenses/agpl-3.0.html)
 
-from odoo import models
+from odoo import models, fields, api
 
 
 class WizStockBarcodesReadPicking(models.TransientModel):
@@ -13,3 +13,17 @@ class WizStockBarcodesReadPicking(models.TransientModel):
         self.lot_id = False
         self.pending_move_ids._compute_qty_done()
         super(WizStockBarcodesReadPicking, self).update_fields_after_process_stock(moves)
+
+class WizCandidatePicking(models.TransientModel):
+    _inherit = "wiz.candidate.picking"
+
+    qty_demand_product = fields.Float(compute='compute_qty_product', store=True)
+    qty_done_product = fields.Float(compute='compute_qty_product', store=True)
+
+    @api.depends('wiz_barcode_id.product_id')
+    def compute_qty_product(self):
+        for record in self:
+            product_id = record.wiz_barcode_id.product_id
+            move_ids = record.picking_id.move_ids_without_package.filtered(lambda x: x.product_id == product_id)
+            record.qty_demand_product = sum(move_ids.mapped('product_uom_qty'))
+            record.qty_done_product = sum(move_ids.mapped('quantity_done'))
