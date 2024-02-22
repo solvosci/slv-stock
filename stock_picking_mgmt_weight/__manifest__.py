bc82f9ac8b77fab0ee22600b5b4f2dee7fb97690
David Alonso // Solvos <david.alonso@solvos.es>
2024-02-22 18:00:26 +0100

    [IMP] stock_picking_mgmt_weight: added supply condition for purchase lines

--- stock_picking_mgmt_weight/__manifest__.py
+++ stock_picking_mgmt_weight/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.1.23.1",
+    "version": "13.0.1.24.0",
     "category": "stock",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": [
