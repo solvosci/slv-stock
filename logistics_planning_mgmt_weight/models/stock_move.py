7316c99c1f096fc9a7ceb328269f756348846e82
David Alonso // Solvos <david.alonso@solvos.es>
2024-03-14 18:01:35 +0100

    [WIP][IMP] logistics_planning_mgmt_weight: direct ticket link for inputs // manual schedules

--- logistics_planning_mgmt_weight/models/stock_move.py
+++ logistics_planning_mgmt_weight/models/stock_move.py
@@ -1,24 +1,61 @@
 # © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
 
-from odoo import models
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
+    aux_picking_partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Ticket partner (auxiliar)",
+        help="""
+        Technical field used as workaround when is required to prefill
+        picking_parter_id with default_ from an action.
+        This happens when we want to create a new ticket from a
+        logistic schedule as draft record
+        """,
+    )
+    aux_picking_vehicle_id = fields.Many2one(
+        comodel_name="vehicle.vehicle",
+        string="Ticket vehicle (auxiliar)",
+        help="""
+        Technical field used as workaround when is required to prefill
+        picking_vehicle_id with default_ from an action.
+        This happens when we want to create a new ticket from a
+        logistic schedule as draft record
+        """,
+    )
+
+    @api.onchange("aux_picking_vehicle_id")
+    def _onchange_aux_picking_vehicle_id(self):
+        if self.aux_picking_vehicle_id:
+            self.picking_vehicle_id = self.aux_picking_vehicle_id
+
+    @api.onchange("aux_picking_partner_id")
+    def _onchange_aux_picking_partner_id(self):
+        if self.aux_picking_partner_id:
+            self.picking_partner_id = self.aux_picking_partner_id
+
     def _prepare_name_get(self):
         name = super()._prepare_name_get()
         self_sudo = self.sudo()
-        sm = self_sudo.browse()
+        if self.net_weight > 0.0:
+            # Tickets => we overwrite default behavior
+            name = "%s (%.3f %s)" % (
+                self.picking_id.name, self.net_weight, self.product_uom.name
+            )
         if self_sudo.picking_vehicle_id:
-            # For outputs
-            sm |= self
-        elif self_sudo.purchase_line_id.related_real_order_line_id:
-            # Ticket inputs
-            sm |= self_sudo.env["stock.move"].search([
-                ("classification_purchase_order_id", "=", self_sudo.purchase_line_id.order_id.id)
-            ])
-        if sm:
-            name = "%s [%s]" % (name, sm.picking_vehicle_id.name)
+            # All cases when there's a license plate
+            name = "%s [%s]" % (name, self_sudo.picking_vehicle_id.name)
         return name
+
+    @api.model
+    def create(self, values):
+        res = super().create(values)
+        if res.logistics_schedule_id:
+            ls = res.logistics_schedule_id.sudo()
+            ls.stock_move_id = res.id
+            ls._onchange_stock_move_id()
+        return res
