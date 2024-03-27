f1753859619925916ca58827535ba3aa4abbacd1
David Alonso // Solvos <david.alonso@solvos.es>
2024-03-27 18:49:43 +0100

    [IMP] stock_inventory_move_custdate: full at date support

---
+++ stock_inventory_move_custdate/models/stock_inventory.py
@@ -0,0 +1,29 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models
+
+
+class StockInventory(models.Model):
+    _inherit = "stock.inventory"
+
+    def action_state_to_in_progress(self):
+        super().action_state_to_in_progress()
+        self.stock_quant_ids.update({"current_inventory_id": self.id})
+
+    def action_state_to_done(self):
+        super().action_state_to_done()
+        self.stock_quant_ids.update({"current_inventory_id": False})
+
+    def action_state_to_draft(self):
+        self.stock_quant_ids.update({"current_inventory_id": False})
+        super().action_state_to_draft()
+
+    def action_view_inventory_adjustment(self):
+        result = super().action_view_inventory_adjustment()
+        result["context"].update({
+            "hide_quantity_at_date": False,
+            # TODO this line should belong to OCA's stock_inventory
+            "default_to_do": True,
+            "default_current_inventory_id": self.id,
+        })
+        return result
--- stock_inventory_move_custdate/models/stock_quant.py
+++ stock_inventory_move_custdate/models/stock_quant.py
@@ -1,10 +1,68 @@
 # © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models
+from odoo import api, fields, models
+from odoo.tools.float_utils import float_compare
 
 
 class StockQuant(models.Model):
     _inherit = "stock.quant"
+
+    current_inventory_id = fields.Many2one(
+        comodel_name="stock.inventory",
+        readonly=True,
+    )
+    quantity_at_date = fields.Float(
+        digits="Product Unit of Measure",
+        compute="_compute_quantity_at_date",
+        store=True,
+    )
+
+    @api.depends("current_inventory_id.date", "quantity")
+    def _compute_quantity_at_date(self):
+        # inventory_ids = self.env["stock.inventory"].search([
+        #     ("stock_quant_ids", "in", self.ids),
+        #     ("state", "=", "in_progress"),
+        # ])
+        quants_inventory_date = self.filtered(lambda x: x.current_inventory_id)
+        for quant in quants_inventory_date:
+            extra_context = {
+                "custdate_inventory": True,
+                "compute_child": False,
+                "location": quant.location_id.id,
+            }
+            inventory_id = quant.current_inventory_id
+            extra_context["to_date"] = inventory_id.date
+            if quant.lot_id:
+                extra_context["lot_id"] = quant.lot_id.id
+            product_id = quant.product_id.with_context(**extra_context)
+            quant.quantity_at_date = product_id.qty_available_lot
+        for quant in (self - quants_inventory_date):
+            quant.quantity_at_date = quant.quantity
+
+    @api.depends("quantity_at_date")
+    def _compute_inventory_diff_quantity(self):
+        for quant in self:
+            quant.inventory_diff_quantity = quant.inventory_quantity - quant.quantity_at_date
+
+    @api.depends("quantity_at_date")
+    def _compute_is_outdated(self):
+        """
+        Outdated recalculation only if is an inventory at date scenario
+        (determined because quantity_date differs from quantity)
+        TODO alternative code: checking current_inventory_id != False
+        """
+        super()._compute_is_outdated()
+        for quant in self.filtered(
+            lambda x: x.product_uom_id and float_compare(
+                x.quantity_at_date,
+                x.quantity,
+                precision_rounding=x.product_uom_id.rounding
+            )
+        ):
+            if quant.product_id and float_compare(quant.inventory_quantity - quant.inventory_diff_quantity, quant.quantity_at_date, precision_rounding=quant.product_uom_id.rounding) and quant.inventory_quantity_set:
+                quant.is_outdated = True
+            else:
+                quant.is_outdated = False    
 
     def _apply_inventory(self):
         """
@@ -15,7 +73,27 @@ class StockQuant(models.Model):
         -     => quants_not_in_invs => directly apply default behavior
         - (3) For those quants not in (2)-set, call _apply_inventory with the 
         -     inventory date they belong to 
-        """
+        """        
+        # Prevent inventory adjustment quant that will be generated to be
+        #  linked to inventory adjustment
+        self = self.with_context(
+            default_to_do=False, default_current_inventory_id=False
+        )
+
+        # ---------------------------------------------------------------------
+        # Alternative code using current_invetory_id field
+        # quants_in_invs = self.filtered(lambda x: x.current_inventory_id and x.to_do)
+        # inv_ids = quants_in_invs.mapped("current_inventory_id")
+        # for inv in inv_ids:
+        #     quants_inv = (
+        #         quants_in_invs.filtered(lambda x: x.current_inventory_id == inv)
+        #     ).with_context(
+        #         stock_move_custom_date=inv.date
+        #     )
+        #     super(StockQuant, quants_inv)._apply_inventory()
+        # super(StockQuant, self - quants_in_invs)._apply_inventory()
+        # ---------------------------------------------------------------------
+
         inv_ids = self.env["stock.inventory"].search([
             ("stock_quant_ids", "in", self.ids),
             ("state", "=", "in_progress"),
@@ -31,3 +109,16 @@ class StockQuant(models.Model):
             )
             super(StockQuant, quants)._apply_inventory()
         super(StockQuant, quants_not_in_invs)._apply_inventory()
+
+    def _get_inventory_fields_write(self):
+        return super()._get_inventory_fields_write() + ["current_inventory_id"]
+
+    @api.model
+    def create(self, vals):
+        """
+        TODO this is not covered by OCA's stock inventory, so we must add it
+        """
+        ret = super().create(vals)
+        if ret.current_inventory_id:
+            ret.current_inventory_id.stock_quant_ids |= ret
+        return ret
