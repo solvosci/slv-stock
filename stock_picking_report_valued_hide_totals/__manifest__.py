f6ed603009e5541fd6c39016c61594e8aab529e3
Anxo82 <angel.rivas@solvos.es>
2024-04-15 12:25:03 +0200

    [ADD] stock_picking_report_valued_hide_totals

---
+++ stock_picking_report_valued_hide_totals/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Picking Report Valued Hide Totals",
+    "summary": """
+        Hide totals summary in valued delivery slip.
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "16.0.1.0.0",
+    "category": "Operations/Stock",
+    "website": "https://github.com/solvosci/slv-stock",
+    "depends": ["stock_picking_report_valued"],
+    "data": [
+        "report/stock_picking_template_hide_totals.xml",
+
+    ],
+    "installable": True,
+}
