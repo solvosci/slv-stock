c540e49fcdcc4065ed0350690b188dc7987a2729
Christian Santamaria <christian.santamaria@solvos.es>
2024-05-02 09:36:05 +0200

    [ADD] stock_picking_batch_split

---
+++ stock_picking_batch_split/__manifest__.py
@@ -0,0 +1,22 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3.0 (http://www.gnu.org/licenses/agpl-3.0.html)
+{
+    "name": "Stock Picking Batch Split",
+    "summary": """
+        Adds split_picking functionalities from picking_batch
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Stock",
+    "website": "",
+    "depends": [
+        "stock_picking_batch_extended",
+        "stock_split_picking",
+    ],
+    "data": [
+        "views/stock_picking_views.xml",
+        "views/stock_picking_batch_views.xml",
+    ],
+    "installable": True,
+}
