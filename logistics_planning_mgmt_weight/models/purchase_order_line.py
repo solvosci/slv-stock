b48b705bf4175c377aea5f4d3c161e55193c1c5f
David Alonso // Solvos <david.alonso@solvos.es>
2024-05-29 17:22:08 +0200

    [IMP] logistics_planning_[purchase|mgmt_weight]: PO and POL fields updated propagation to LSs support
    With this improvement, those logistics schedules fields that come from
    purchase order and/or purchase line are automatically updated when
    origin fields are updated

--- logistics_planning_mgmt_weight/models/purchase_order_line.py
+++ logistics_planning_mgmt_weight/models/purchase_order_line.py
@@ -16,3 +16,10 @@ class PurchaseOrderLine(models.Model):
             "supply_condition_id": self.supply_condition_id.id or False,
         })
         return values
+
+    def _update_logistics_schedules_dict(self):
+        ret = super()._update_logistics_schedules_dict()
+        ret.update({
+            "supply_condition_id": "supply_condition_id",
+        })
+        return ret
