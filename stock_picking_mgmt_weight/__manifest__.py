b852b8d777b148c8df34bfd935280606caed406d
David Alonso // Solvos <david.alonso@solvos.es>
2024-05-30 09:24:49 +0200

    [IMP] stock_picking_mgmt_weight: ticket weight capture - obtain last weight read by other processes
    For scale integration in ticketing creation we have the possibility
    of simply get last weight registered by other processes (weight readings
    from ticket form & list views, 1 second period from every page instance),
    that is enough as recent weight.
    With this improvement instant weight is not captured, that could lead to
    a better performance and prevent concurrent access that should fire
    frequent errors.

--- stock_picking_mgmt_weight/__manifest__.py
+++ stock_picking_mgmt_weight/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.1.24.3",
+    "version": "13.0.1.25.0",
     "category": "stock",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": [
