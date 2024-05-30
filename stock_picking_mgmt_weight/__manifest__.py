cfb19a91d99455e9734a32c36f0bbfeba4b8dc15
David Alonso // Solvos <david.alonso@solvos.es>
2024-05-30 09:02:38 +0200

    [FIX] stock_picking_mgmt_weight: ticket creation - use net date for numbering - default case
    When a ticket is created without filling any weight (and dates), no date
    is selected for ticket number determination, and an error is raised.
    This is the usual case when e.g. "capture gross" button is clicked
    during ticket creation (not manual creation).
    This fix was introduced by #77.

--- stock_picking_mgmt_weight/__manifest__.py
+++ stock_picking_mgmt_weight/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.1.24.2",
+    "version": "13.0.1.24.3",
     "category": "stock",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": [
