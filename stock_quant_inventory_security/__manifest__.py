ec824bc6e84520bfdacfffcbff7e544cd997ae02
David Alonso // Solvos <david.alonso@solvos.es>
2024-08-05 14:22:48 +0200

    [ADD] stock_quant_inventory_security
    Adds a new group that is enabled to make Inventory Adjustments.
    
    This new group automatically implies being Inventory Administrator.

---
+++ stock_quant_inventory_security/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Stock Quants - new Inventory Adjustments group",
+    "summary": """
+        Adds a new group that is enabled to make Inventory Adjustments
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Inventory",
+    "website": "https://github.com/solvosci/slv-stock",
+    "depends": ["stock"],
+    "data": [
+        "security/stock_quant_inventory_security.xml",
+        "views/stock_quant_inventory_security_menus.xml",
+    ],
+}
