95c00eaebb38cdbd640a5251cd364f3bac28d612
David Alonso // Solvos <david.alonso@solvos.es>
2025-02-20 18:24:51 +0100

    [ADD] stock_picking_delivery_slip_summarized
    Default delivery slip report, when done, shows a line for each Lot/Serial
    number consumed, for tracked products.
    
    With this addon, the possibility to obtain a delivery slip summarized by
    quantity for each line is added. It is achieved with a new "Print (summarized)"
    button available within picking form.

---
+++ stock_picking_delivery_slip_summarized/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Stock Picking Report  - Delivery Slip Summarized",
+    "summary": """
+        Adds the posibility to obtain a delivery slip summarized for each line
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Inventory",
+    "website": "https://github.com/solvosci/slv-stock",
+    "depends": ["stock"],
+    "data": [
+        "report/stock_picking_templates.xml",
+        "report/stock_picking_report.xml",
+        "views/stock_picking_views.xml",
+    ],
+}
---
+++ stock_picking_delivery_slip_summarized/readme/DESCRIPTION.rst
@@ -0,0 +1,6 @@
+Default delivery slip report, when done, shows a line for each Lot/Serial
+number consumed, for tracked products.
+
+With this addon, the possibility to obtain a delivery slip summarized by
+quantity for each line is added. It is achieved with a new "Print (summarized)"
+button available within picking form.
