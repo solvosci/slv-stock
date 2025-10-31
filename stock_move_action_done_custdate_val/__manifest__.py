33796c933c6382ea7d1d583b0ba04ea57f4bf66b
Laura Pérez // Solvos <laura.perez@solvos.es>
2025-10-31 11:47:23 +0100

    [MIG] stock_move_action_done_custdate_val: Migration to 17.0

--- stock_move_action_done_custdate_val/__manifest__.py
+++ stock_move_action_done_custdate_val/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "stock",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": ["stock_move_action_done_custdate", "stock_account"],
