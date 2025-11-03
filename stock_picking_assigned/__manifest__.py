ce23567ec90a2b85b0a30c780ff13a4deceaf118
Christian Santamaria <christian.santamaria@solvos.es>
2025-11-03 10:54:14 +0100

    [IMP] stock_picking_assigned: exclude 0kg in lot field

--- stock_picking_assigned/__manifest__.py
+++ stock_picking_assigned/__manifest__.py
@@ -14,11 +14,13 @@
     ],
     "data": [
         "security/ir.model.access.csv",
+        "data/ir_cron_data.xml",
         "data/report_paperformat_data.xml",
         "views/stock_picking_assigned_menu.xml",
         "reports/stock_picking_assigned_report.xml",
         "reports/stock_picking_assigned_template.xml",
         "views/stock_picking_views.xml",
+        "views/stock_production_lot_views.xml",
         "views/res_partner_views.xml",
         "views/stock_picking_assign_history_views.xml",
         "wizard/sp_assigned_wizard_views.xml",
