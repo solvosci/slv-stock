22ccf98a1551c8de8c0d4116d5fd1c3e7e0fdf9c
Carlos García <carlos.garcia@solvos.es>
2025-11-25 11:48:51 +0100

    [ADD] product_tracking_chatter
    Allows tracking in the chatter of changes made to the tracking field on products

---
+++ product_tracking_chatter/__manifest__.py
@@ -0,0 +1,14 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Product Tracking",
+    "summary": (
+        """Allows tracking in the chatter of changes made to the tracking field on products"""
+    ),
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    'category': 'Stock',
+    "website": "https://github.com/solvosci/slv-stock",
+    "depends": ["stock"],
+}
