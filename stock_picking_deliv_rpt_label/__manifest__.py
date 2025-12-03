4f74db1dab5c606d68624137c09a3dc9b86e3ca9
Iria Alonso <iria.alonso@solvos.es>
2025-12-03 13:37:51 +0100

    [ADD] stock_picking_deliv_rpt_label Adds 'Delivery Note' as picking delivery report label before picking number

---
+++ stock_picking_deliv_rpt_label/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Stock Picking Delivery Report Label",
+    "summary": """
+        Adds 'Delivery Note' as picking delivery report label before picking number
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "17.0.1.0.0",
+    "category": "Inventory",
+    "website": "https://github.com/solvosci/slv-stock",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "report/stock_picking_template.xml",
+    ],
+    "installable": True,
+}
---
+++ stock_picking_deliv_rpt_label/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+Adds 'Delivery Note' as picking delivery report label before picking number
