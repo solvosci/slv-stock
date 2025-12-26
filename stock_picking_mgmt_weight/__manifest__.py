ec70619b8cfb247135ad20e7ec2a5f8d712ef25b
David Alonso // Solvos <david.alonso@solvos.es>
2025-12-26 19:08:01 +0100

    [FIX] stock_picking_mgmt_weight: pending column_invisible stuff from v13 migration

--- stock_picking_mgmt_weight/__manifest__.py
+++ stock_picking_mgmt_weight/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "17.0.1.2.0",
+    "version": "17.0.1.2.1",
     "category": "stock",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": [
