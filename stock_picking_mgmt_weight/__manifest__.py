1b106d5f3a1a051ce37a096e8a4c37855e64c0a5
David Alonso // Solvos <david.alonso@solvos.es>
2026-01-13 17:58:17 +0100

    [FIX] stock_picking_mgmt_weight: missing invoice status extended management in purchase tree views

--- stock_picking_mgmt_weight/__manifest__.py
+++ stock_picking_mgmt_weight/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "17.0.1.2.1",
+    "version": "17.0.1.2.2",
     "category": "stock",
     "website": "https://github.com/solvosci/slv-stock",
     "depends": [
