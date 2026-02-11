8f887c3c9c56f52f1d5251d7a1fcf9b44639111f
ChristianSantamaria <christiancameselle@hotmail.com>
2026-02-11 09:42:09 +0100

    [MIG] stock_picking_mgmt_weight_classification_images: Migration to 17.0

--- stock_picking_mgmt_weight_classification_images/__manifest__.py
+++ stock_picking_mgmt_weight_classification_images/__manifest__.py
@@ -1,13 +1,13 @@
 # © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
-# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     'name': 'Stock Picking Move Weight Classification Images',
     'summary': '''
         Allows adding images in the classification of a weighing scale.
     ''',
     'author': 'Solvos',
-    'license': 'LGPL-3',
-    'version': '13.0.1.0.0',
+    'license': 'AGPL-3',
+    'version': '17.0.1.0.0',
     'category': 'stock',
     'website': 'https://github.com/solvosci/slv-stock',
     'depends': [
