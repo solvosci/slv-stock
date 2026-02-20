10bccdfc02452c24c79ea62e713f8c52df0394f4
ChristianSantamaria <christiancameselle@hotmail.com>
2026-02-20 10:16:52 +0100

    [ADD] stock_barcodes_datamatrix_mh10
    Scan ANSI MH10 Datamatrix barcodes in the Stock Barcode Wizard and
    process them with GS separators using the following configurable fields:
        - Product (1P)
        - Lot (1T)
        - Quantity (Q)

---
+++ stock_barcodes_datamatrix_mh10/models/stock_barcodes_option.py
@@ -0,0 +1,14 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class StockBarcodesOption(models.Model):
+    _inherit = "stock.barcodes.option"
+
+    ansi_datamatrix_mh10 = fields.Selection([
+        ('Q', 'Quantity (Q)'),
+        ('1P', 'Product Code (1P)'),
+        ('1T', 'Lot Code (1T)'),
+    ], string="ANSI MH10", help="Configure this field to enable processing of the corresponding AI in ANSI MH10 Datamatrix barcodes.")
---
+++ stock_barcodes_datamatrix_mh10/wizard/__init__.py
@@ -0,0 +1,2 @@
+from . import stock_barcodes_read
+from . import stock_barcodes_read_picking
